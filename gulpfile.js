e3b77d0b99ab81ea4b5db255010841a5252d22d4
joshdrink <git@joshbeveridge.ca>
2020-02-23 10:44:35 -0500

    Gulp and NPM

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+// =============================================================================
+
+  // Clone & Jekyll Boilerplate
+  // Authored by Josh Beveridge
+
+// =============================================================================
+
+"use strict";
+
+// Requirements
+const { series, parallel, src, dest, watch } = require('gulp');
+const sass = require('gulp-sass');
+const browsersync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+// Tasks
+    // Browser Sync
+    function browserSync(done) {
+        browsersync.init({
+            server: {
+                baseDir: "_site"
+            },
+        });
+        done();
+    };
+    // BrowserSync Reload
+    function browserSyncReload(done) {
+        return src('_site/*.html')
+        .pipe(browsersync.reload({
+            stream: true
+        }));
+    }
+    // Clone JS
+    function moveCloneJS() {
+        return src('node_modules/clone-framework/dist/js/clone.min.js')
+        .pipe(dest('assets/js/clone'));
+    }
+    // Sass
+    function compile() {
+        return src('assets/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(dest('assets/css'));
+    }
+    // Watch
+    function watchFiles() {
+        watch('assets/scss/**/*.scss', series(compile, browserSyncReload));
+        watch('assets/js/**/*.js', series(compile, browserSyncReload));
+        watch('assets/img/**/*.js', series(compile, browserSyncReload));
+    }
+
+// Exports
+    exports.watch = series(moveCloneJS, compile, parallel(browserSync, watchFiles));
+    exports.default = series(moveCloneJS, compile, parallel(browserSync, watchFiles));
